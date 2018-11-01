c4e7e275329e26fe0d4e5ba506c9c4406c2225bf
rejbo.dawid@gmail.com <rejbo.dawid@gmail.com>
2018-11-01 10:00:08 +0100

    first commit

---
+++ src/main/java/com/Rejbo/Dawid/investmentportfolio/InvestmentPortfolioApplication.java
@@ -0,0 +1,12 @@
+package com.Rejbo.Dawid.investmentportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentPortfolioApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Rejbo/Dawid/investmentportfolio/InvestmentPortfolioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Rejbo.Dawid.investmentportfolio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvestmentPortfolioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
